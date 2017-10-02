52c8c7ab198c5eea8228a930b6003ecd4ddd03cf
Felipe Pelichero <pelichero_13@hotmail.com>
2017-10-02 20:44:33 -0300

    Initial commit

---
+++ src/main/java/edu/caelum/estudos/java8napratica/Java8napraticaApplication.java
@@ -0,0 +1,12 @@
+package edu.caelum.estudos.java8napratica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8napraticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8napraticaApplication.class, args);
+	}
+}
